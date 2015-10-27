8536c48022245b6f78d90b623c646f5d073a50ce
weixing <weixing@tudou.com>
2015-10-27 15:11:50 +0800

    map 中的操作

---
+++ goMap1.go
@@ -0,0 +1,18 @@
+// goMap1
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var m = make(map[string]int)
+	m["1"] = 1
+	//修改map值
+	m["1"] = 2
+
+	delete(m, "2") //删除map中的元素
+	v, ok := m["1"]
+
+	fmt.Println(v, ok)
+}
